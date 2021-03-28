b29f45697431248204ecaebf62632a88f24a5f61
Ahmed Gheith <ag@gheith.com>
2021-03-28 08:51:57 -0500

    logger

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,11 +1,15 @@
 object main {
 
+  val LOG = false
+
   def logger(what: String, data: Any) = {
+    if (LOG) {
       val d = data match {
-          case a:Array[_] => a.toList.toString
-          case x => s"${x} : ${x.getClass.getName}"
+        case a: Array[_] => a.toList.toString
+        case x => s"${x} : ${x.getClass.getName}"
       }
       println(s"$what $d")
+    }
   }
 
   def main(args: Array[String]): Unit = {
@@ -19,8 +23,13 @@ object main {
     os.write(data / "a" / "f", "f")
     os.write(data / "b" / "g", "g")
 
+    //println("giving the file system time to settle")
+    //Thread.sleep(5000)
+
+    //println("watching")
     os.watch.watch(Seq(data), println, logger)
 
+    //println("removing")
     os.remove.all(data)
 
     Thread.sleep(1000000)
